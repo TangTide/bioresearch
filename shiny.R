552cf28235c2a73d9585072bf5b7bb3065f37a26
TangTide <89908277+TangTide@users.noreply.github.com>
2022-12-20 23:12:39 +0800

    Add files via upload

---
+++ shiny.R
@@ -0,0 +1,8 @@
+install.packages("remotes")         
+
+remotes::install_github("massimoaria/bibliometrix")
+
+remotes::install_github("massimoaria/bibliometrixData")
+
+library(bibliometrix)
+biblioshiny()
